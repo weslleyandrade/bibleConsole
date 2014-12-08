8362d5f34d27b2aa8054b3a77909b96dcb256cce
Weslley Andrade <weslleyandrade@gmail.com>
2014-12-08 21:01:20 -0200

    add. reading by args

--- bible.go
+++ bible.go
@@ -9,28 +9,43 @@ import (
 
 func main() {
 	prov := &provider.Biblebox{}
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
 
-		fmt.Print("Livro: ")
-		scanner.Scan()
-		book := scanner.Text()
+	lenArgs := len(os.Args)
 
-		fmt.Printf("%s > Capitulo: ", book)
-		scanner.Scan()
-		chapter := scanner.Text()
-
-		fmt.Printf("%s > %s > Versiculo: ", book, chapter)
-		scanner.Scan()
-		verse := scanner.Text()
-
-		if verse == "" {
-			fmt.Printf("%s > %s : \n", book, chapter)
-			fmt.Println(prov.GetChapter(book, chapter))
-		} else {
-			fmt.Printf("%s > %s > %s : \n", book, chapter, verse)
+	if lenArgs > 2 {
+		book := os.Args[1]
+		chapter := os.Args[2]
+		if lenArgs > 3 {
+			verse := os.Args[3]
 			fmt.Println(prov.GetVerses(book, chapter, verse))
+
+		} else {
+			fmt.Println(prov.GetChapter(book, chapter))
+		}
+
+	} else {
+		scanner := bufio.NewScanner(os.Stdin)
+		for {
+
+			fmt.Print("Livro: ")
+			scanner.Scan()
+			book := scanner.Text()
+
+			fmt.Printf("%s > Capitulo: ", book)
+			scanner.Scan()
+			chapter := scanner.Text()
+
+			fmt.Printf("%s > %s > Versiculo: ", book, chapter)
+			scanner.Scan()
+			verse := scanner.Text()
+
+			if verse == "" {
+				fmt.Printf("%s > %s : \n", book, chapter)
+				fmt.Println(prov.GetChapter(book, chapter))
+			} else {
+				fmt.Printf("%s > %s > %s : \n", book, chapter, verse)
+				fmt.Println(prov.GetVerses(book, chapter, verse))
+			}
 		}
 	}
-
 }
